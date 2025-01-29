d00b49d84b4c88f0a0eedf9cfa376a0bdb2883e0
Taven Tran <taventran@Tavens-MacBook-Pro.local>
2025-01-29 15:16:55 -0600

    Added random text to textarea

--- script.js
+++ script.js
@@ -4,7 +4,15 @@ function generate_words() {
   fetch('./words.json')
       .then((response) => response.json())
       .then((json) => words = json);
-  console.log(words);
+
+  const randomElements = getRandomElements(words, 100); // Get 3 unique random elements
+  const textarea = document.getElementById("typing");
+
+  for (let i = 0; i < randomElements.size; i++) {
+    // Append the random word to the textarea
+    let currentText = textarea.value;
+    textarea.value = currentText + randomWord + " ";
+  }
 }
 
 generate_words()
