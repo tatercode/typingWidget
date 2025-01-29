3c1ca1d6f18c260060deacb22ee0278b7eb8d29b
Taven Tran <taventran@Tavens-MacBook-Pro.local>
2025-01-29 15:57:06 -0600

    Fixed?

--- script.js
+++ script.js
@@ -3,14 +3,16 @@ function generate_words() {
   let words;
   fetch('./words.json')
       .then((response) => response.json())
-      .then((json) => words = json);
-  
-  const textarea = document.getElementById("typing");
+      .then((words) => {
+    
+        const textarea = document.getElementById("typing");
 
-  for (let i = 0; i < 100; i++) {
-    const randomWord = words[Math.floor(Math.random() * words.length)];
-    currentText += randomWord + " ";
-  }
+        for (let i = 0; i < 100; i++) {
+          const randomWord = words[Math.floor(Math.random() * words.length)];
+          currentText += randomWord + " ";
+        }
+      })
+      .catch((error) => console.error("Error getting words", error);
 }
 
 generate_words()
